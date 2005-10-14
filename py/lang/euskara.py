6dcd573f28d16762b08f435f9033aab5d4b0bbf9
monsterkodi <monsterkodi@users.sourceforge.net>
2005-10-14 09:28:04 +0000

    euskara translations by 3ARRANO.com

---
+++ py/lang/euskara.py
@@ -0,0 +1,74 @@
+# -*- encoding: utf-8 -*-
+# Euskara versions of kiki texts 
+# translation by 3ARRANO.com
+
+lang['euskara'] = {
+
+# game title
+
+"kiki the nano bot": "kiki the nano bot",
+
+# menu 
+
+"new game": "joku berria",
+"load level": "maila eskuratu",
+"next level": "hurrengo maila",
+"restart": "berriz hasi",
+"setup": "ezarpenak",
+"story": "istorioa",
+"demo": "erakustaldia",
+"about": "honen gainean",
+"help": "laguntza",
+"quit": "irten",
+"back": "atzera",
+
+# about
+
+"version": "bertsioa:",
+"programmed by": "programatzailea:",
+"sounds from": "soinuak:",
+"thanks to": "eskerrak:",
+"visit": "ikusi",
+"for more games": "joku gehiagorako",
+"main menu": "hautesleihoa",
+
+# story 
+
+"STORY_PAGE_1": "behin batean, nano\nmunduan errobot\ntxiki batzuk bizi\nziren.\n\nbizitza artifizial alai bat\nbizi zuten egilearentzako baliabide\n bilketarekin lanpeturik,\nhark egin baitzituen errobot\ntxiki guztiak.",
+"STORY_PAGE_2": "baina halako batean,\nindar parasito batek\negilearen kontrol gailu\nnagusia suntsitu zuen.\n\ngeroztik egileak gaizki\nfuntzionatzen du\neta errobot inozo eta alferrak\nsoilik sortzen ditu,\nzeintzuek elkar tirokatzen duten\nhonela nano mundua suntsituz.",
+"STORY_PAGE_3": "zure eginbeharra kiki, bizi\nden errobot on bakarra,\nlaguntzea da, egilea konpon dezan.\n\nzorte on!",
+
+# setup
+
+"language": "hizkuntza",
+"off": "ez",
+"on": "bai",
+"sound": "soinua",
+"volume": "ozentasuna",
+"fullscreen": "osoko ikuskera",
+"fov": "ikuskera",
+"speed": "abiadura",
+"keyboard setup": "teklak zehaztu",
+"save": "gorde",
+
+# keyboard setup 
+
+"$no_itemkeyboard setup": "$no_itemteklak zehaztu" # don't translate $no_item
+"$no_item": "$no_item", # don't change this
+
+"move forward": "aurrera egin",
+"move backward": "atzera egin",
+"turn left": "ezkerrera biratu",
+"turn right": "eskumara biratu",
+"jump": "jauzi",
+"push": "sakatu",
+"shoot": "tirokatu",
+"change view": "ikuskera aldatu",
+"look up": "gora begiratu",
+"look down": "behera begiratu",
+      
+"press the new key": "sakatu tekla berria",
+"action without key": "tekla gabeko ekintza",
+"back to setup": "atzera ezarpenetara",
+}
+                  
