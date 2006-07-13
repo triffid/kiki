fd57a72efdf885d949ede41a97d597a56c331042
monsterkodi <monsterkodi@users.sourceforge.net>
2006-07-13 10:21:26 +0000

    *** empty log message ***

---
+++ py/lang/francaise.py
@@ -0,0 +1,77 @@
+# -*- encoding: utf-8 -*-
+# Versions Francaise de kiki texts
+# translation by philrich123.free.fr
+
+lang['francais'] = {
+
+# game title
+
+"kiki the nano bot": "kiki le nano robot",
+
+# menu
+
+"new game": "nouvelle Partie",
+"load level": "charger le niveau",
+"next level": "niveau suivant",
+"restart": "redemarrer",
+"setup": "configuration",
+"story": "histoire",
+"demo": "demonstration",
+"about": "a propos",
+"help": "aide",
+"quit": "quitter",
+"back": "retour",
+
+# about
+
+"version": "version",
+"programmed by": "programmation de",
+"sounds from": "les sons sont de",
+"thanks": "remerciements",
+"visit": "visiter",
+"for more games": "pour plus de jeux",
+"main menu": "menu principal",
+
+# story
+
+"STORY_PAGE_1": "Il etait une fois,\ndes tout petis robots\nvivants dans un tout\npetit monde.\n\nIls vivaient une vie\ntres agitee, collectant\nles ressources necessaires\na la fabrication d'autres robots\nminuscules.",
+"STORY_PAGE_2": "Mais un jour,\nun virus detruisit le programme\nde l'appareil a fabriquer les nouveaux\nrobots.\n\nDepuis lors il \nfonctionne mal\net ne produit plus\nque des affreux petits robots\nstupides qui ne pense qu'a\ntout detruire.",
+"STORY_PAGE_3": "Votre tache est d'aider kiki,\nle seul robot sain qui reste,\na depanner le programme.\n\nbonne chance!",
+
+# statistics
+
+"STATISTICS_TEXT": "$scale(1.5)statistiques\n\nvous avez resolu %d des %d niveaux\n\nvotre score actuel est\n\n$scale(2.0)%d",
+
+# setup
+
+"language": "langue",
+"off": "non",
+"on": "oui",
+"sound": "son",
+"volume": "volume",
+"fullscreen": "plein ecran",
+"fov": "fov",
+"speed": "vitesse",
+"keyboard setup": "configuration du clavier",
+"save": "sauver",
+
+# keyboard setup
+
+"$no_itemkeyboard setup": "$no_itemconfiguration du clavier", # don't translate $no_item
+"$no_item": "$no_item", # don't change this
+
+"move forward": "aller en avant",
+"move backward": "aller en arriere",
+"turn left": "tourner a gauche",
+"turn right": "tourner a droite",
+"jump": "sauter",
+"push": "pousser",
+"shoot": "tirer",
+"change view": "changer de vue",
+"look up": "regarder vers le haut",
+"look down": "regarder vers le bas",
+
+"press the new key": "appuyer sur la nouvelle touche",
+"action without key": "action sans touche",
+"back to setup": "retour a la configuration",
+}
