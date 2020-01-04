76d68beb13024473ca8d3e79edebd50e8760d4b3
Anna Domini <Mistress-Anna@email.invalid>
2020-01-04 23:08:16 +0100

    KikiPython: importing the internal lib "_kiki" changed in python3
    The Python part of the app is made of 2 parts:
    - "_kiki" C extension backend generated by SWIG
    - "kiki" frontend written in Python
    
    With python3, the init function has changed and is now PyInit__kiki. The
    python API also changed as Py_InitModule disappeared, and SWIG uses
    PyModule_Create.
    
    The replacement code was found at:
    https://docs.python.org/3/extending/extending.html#the-module-s-method-table-and-initialization-function

--- SWIG/KikiPy_wrap.h
+++ SWIG/KikiPy_wrap.h
@@ -6,7 +6,7 @@
 #ifndef __KikiPy_wrap
 #define __KikiPy_wrap
 
-extern "C" void init_kiki ();
+extern "C" PyObject* PyInit__kiki ();
 
 #endif
 
--- src/main/KikiPython.cpp
+++ src/main/KikiPython.cpp
@@ -117,11 +117,14 @@ void KikiPython::initPython ()
 	putenv (pypath.c_str());
 #endif
 
+    PyImport_AppendInittab("_kiki", &PyInit__kiki);
+
     Py_Initialize();
 
     std::string kikipy_path = kFileNativePath(Controller.getKikiHome() + "py/kiki.py");
-    
-    init_kiki();
+
+    PyImport_ImportModule("_kiki");
+
     
     FILE * pythonFile = fopen (kikipy_path.c_str(), "r");
     
